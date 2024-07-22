6efa487a5a23a608e10dc38fe94bb4f199069f8c
NguyenLeHieu <nguyenhieu157380@gmail.com>
2024-07-23 01:15:56 +0700

    Initial Commit

---
+++ Oto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Oto
+//
+//  Created by Nguyễn Văn Hiếu on 23/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
